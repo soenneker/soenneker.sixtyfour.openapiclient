d2008ae503909aa661f65ee7bb76b9b55802e11b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-17 17:52:45 +0000

    Automated update

--- src/Soenneker.Sixtyfour.OpenApiClient/EnrichCompanyV2/EnrichCompanyV2RequestBuilder.cs
+++ src/Soenneker.Sixtyfour.OpenApiClient/EnrichCompanyV2/EnrichCompanyV2RequestBuilder.cs
@@ -34,7 +34,7 @@ namespace Soenneker.Sixtyfour.OpenApiClient.EnrichCompanyV2
         {
         }
         /// <summary>
-        /// Enrich Company Endpoint
+        /// Enrich Company V2 Endpoint
         /// </summary>
         /// <returns>A <see cref="UntypedNode"/></returns>
         /// <param name="body">The request body</param>
@@ -59,7 +59,7 @@ namespace Soenneker.Sixtyfour.OpenApiClient.EnrichCompanyV2
             return await RequestAdapter.SendAsync<UntypedNode>(requestInfo, UntypedNode.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
-        /// Enrich Company Endpoint
+        /// Enrich Company V2 Endpoint
         /// </summary>
         /// <returns>A <see cref="RequestInformation"/></returns>
         /// <param name="body">The request body</param>
--- src/Soenneker.Sixtyfour.OpenApiClient/EnrichCompanyV2Async/EnrichCompanyV2AsyncRequestBuilder.cs
+++ src/Soenneker.Sixtyfour.OpenApiClient/EnrichCompanyV2Async/EnrichCompanyV2AsyncRequestBuilder.cs
@@ -34,7 +34,7 @@ namespace Soenneker.Sixtyfour.OpenApiClient.EnrichCompanyV2Async
         {
         }
         /// <summary>
-        /// Enrich Company Async Endpoint
+        /// Enrich Company V2 Async Endpoint
         /// </summary>
         /// <returns>A <see cref="UntypedNode"/></returns>
         /// <param name="body">The request body</param>
@@ -59,7 +59,7 @@ namespace Soenneker.Sixtyfour.OpenApiClient.EnrichCompanyV2Async
             return await RequestAdapter.SendAsync<UntypedNode>(requestInfo, UntypedNode.CreateFromDiscriminatorValue, errorMapping, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
-        /// Enrich Company Async Endpoint
+        /// Enrich Company V2 Async Endpoint
         /// </summary>
         /// <returns>A <see cref="RequestInformation"/></returns>
         /// <param name="body">The request body</param>
