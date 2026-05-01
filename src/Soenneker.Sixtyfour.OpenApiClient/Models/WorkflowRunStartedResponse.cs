40fa718092955fcfaebf32cee68ee5b7c25d3fc4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-01 18:01:08 +0000

    Automated update

--- src/Soenneker.Sixtyfour.OpenApiClient/Models/WorkflowRunStartedResponse.cs
+++ src/Soenneker.Sixtyfour.OpenApiClient/Models/WorkflowRunStartedResponse.cs
@@ -15,6 +15,14 @@ namespace Soenneker.Sixtyfour.OpenApiClient.Models
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>True when the request was deduplicated via Idempotency-Key header.</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started? AlreadyStarted { get; set; }
+#nullable restore
+#else
+        public global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started AlreadyStarted { get; set; }
+#endif
         /// <summary>Cache configuration; only set by the custom-cache endpoint.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -72,6 +80,7 @@ namespace Soenneker.Sixtyfour.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "already_started", n => { AlreadyStarted = n.GetObjectValue<global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started>(global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started.CreateFromDiscriminatorValue); } },
                 { "cache_config", n => { CacheConfig = n.GetObjectValue<UntypedNode>(UntypedNode.CreateFromDiscriminatorValue); } },
                 { "job_id", n => { JobId = n.GetStringValue(); } },
                 { "status", n => { Status = n.GetStringValue(); } },
@@ -85,11 +94,74 @@ namespace Soenneker.Sixtyfour.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+            writer.WriteObjectValue<global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started>("already_started", AlreadyStarted);
             writer.WriteObjectValue<UntypedNode>("cache_config", CacheConfig);
             writer.WriteStringValue("job_id", JobId);
             writer.WriteStringValue("status", Status);
             writer.WriteStringValue("workflow_id", WorkflowId);
             writer.WriteAdditionalData(AdditionalData);
+        }
+        /// <summary>
+        /// Composed type wrapper for classes <see cref="bool"/>, <see cref="global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1"/>
+        /// </summary>
+        [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+        public partial class WorkflowRunStartedResponse_already_started : IComposedTypeWrapper, IParsable
+        {
+            /// <summary>Composed type representation for type <see cref="bool"/></summary>
+            public bool? Boolean { get; set; }
+            /// <summary>Composed type representation for type <see cref="global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1"/></summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+            public global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1? WorkflowRunStartedResponseAlreadyStartedMember1 { get; set; }
+#nullable restore
+#else
+            public global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1 WorkflowRunStartedResponseAlreadyStartedMember1 { get; set; }
+#endif
+            /// <summary>
+            /// Creates a new instance of the appropriate class based on discriminator value
+            /// </summary>
+            /// <returns>A <see cref="global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started"/></returns>
+            /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+            public static global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started CreateFromDiscriminatorValue(IParseNode parseNode)
+            {
+                if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+                var result = new global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse.WorkflowRunStartedResponse_already_started();
+                if(parseNode.GetBoolValue() is bool booleanValue)
+                {
+                    result.Boolean = booleanValue;
+                }
+                else {
+                    result.WorkflowRunStartedResponseAlreadyStartedMember1 = new global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1();
+                }
+                return result;
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+            public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+            {
+                if(WorkflowRunStartedResponseAlreadyStartedMember1 != null)
+                {
+                    return ParseNodeHelper.MergeDeserializersForIntersectionWrapper(WorkflowRunStartedResponseAlreadyStartedMember1);
+                }
+                return new Dictionary<string, Action<IParseNode>>();
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// </summary>
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            public virtual void Serialize(ISerializationWriter writer)
+            {
+                if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+                if(Boolean != null)
+                {
+                    writer.WriteBoolValue(null, Boolean);
+                }
+                else {
+                    writer.WriteObjectValue<global::Soenneker.Sixtyfour.OpenApiClient.Models.WorkflowRunStartedResponse_already_startedMember1>(null, WorkflowRunStartedResponseAlreadyStartedMember1);
+                }
+            }
         }
     }
 }
